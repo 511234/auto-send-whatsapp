13e79c80025753201a6257ff38fdd22096ffd07c
511234 <lulutheflaneur@gmail.com>
2022-08-27 17:58:40 +0800

    👌 IMPROVE: clean code

--- python/main.py
+++ python/main.py
@@ -1,40 +1,30 @@
 import json
 import math
 import pandas
+import pyautogui
 import time
+
 from operator import itemgetter
 from webbrowser import open
 from urllib.parse import quote
-
-import pyautogui
 
 from helper.variable_getter import get_message, get_name, get_phone
 from helper.check_config import check_config
 from helper.sanitizer import sanitize_url, sanitize_phone
 
-WIDTH, HEIGHT = pyautogui.size()
-config_keys = ("column_names", "sheet_name", "url")
-
-
 def main():
     try:
-        column_names, sheet_name, url = {**check_config()}.values()
+        _column_names, sheet_name, url = {**check_config()}.values()
         sanitized_url = sanitize_url(sheet_name, url)
-        column_names_list = json.loads(column_names)
-        print(column_names_list)
-        print(type(column_names_list))
 
         raw_list = pandas.read_csv(sanitized_url).to_dict("records")
-        print(raw_list)
         whatsapp_list = [
             {**raw_people, "phone": str(int(raw_people.get("phone")))}
             for raw_people in raw_list
             if raw_people.get("phone") and not math.isnan(raw_people.get("phone"))
         ]
-        print("whatsapp_list")
-        print(whatsapp_list)
 
-        message = "lulu testing auto whatsapp"
+
         for whatsapp_receiver in whatsapp_list:
             message = get_message(whatsapp_receiver) or "default message"
             name = get_name(whatsapp_receiver)
