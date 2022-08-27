6db63a770a4a1260d5d9815daaf20e1d2affc56b
511234 <lulutheflaneur@gmail.com>
2022-08-27 16:03:26 +0800

    👌 IMPROVE: handle phone number without code

--- python/helper/sanitizer.py
+++ python/helper/sanitizer.py
@@ -1,8 +1,15 @@
 from re import search
+import re
 from urllib.parse import quote
 
 
-def sanitize_url(sheet_name: str, url: str):
+def sanitize_phone(phone: str) -> str:
+    if re.match("^\+852", phone):
+        return phone
+    return f"+852{phone}"
+
+
+def sanitize_url(sheet_name: str, url: str) -> str:
     # Get sheet ID between /d/ and /
     sheet_id = search(r"\/d\/(.*?)\/", url).group(1)
     return f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={quote(sheet_name)}"
