54ec4ed1caadc54f55088761ceddb3b4407d9822
511234 <lulutheflaneur@gmail.com>
2022-09-03 11:09:42 +0800

    👌 IMPROVE: remove unused

--- python/main.py
+++
@@ -1,6 +0,0 @@
-from helper.check_config import check_config
-from send_whatsapp import send
-
-if __name__ == "__main__":
-    _column_names, sheet_name, url = {**check_config()}.values()
-    send(sheet_name, url)
